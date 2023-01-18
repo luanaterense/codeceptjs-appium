b5959140f971bdc3785f1a9aab37b7f49aa678ba
Luana Matias Terense <luana.r.matias@gmail.com>
2023-01-18 14:59:09 -0300

    Ajustes no codecept.conf.ts apagando llinhas desnecessárias

--- codecept.conf.ts
+++ codecept.conf.ts
@@ -1,7 +1,7 @@
 import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
+//setHeadlessWhen(process.env.HEADLESS);
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
 setCommonPlugins();
